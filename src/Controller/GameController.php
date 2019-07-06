97a369d8b08003267cb490b5432efd82fb403e9d
Val <valentin.antoingmail.com>
2019-07-06 20:18:33 +0200

    Create platform template

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -14,4 +14,12 @@ class GameController extends AbstractController
     {
         return $this->render('game/game.html.twig');
     }
+
+    /**
+     * @Route("/platforms", name="platforms")
+     */
+    public function platforms() 
+    {
+        return $this->render('game/platforms.html.twig');
+    }
 }
