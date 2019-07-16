8af82dea274ee90cec4a2496951dea89558706bf
Val <valentin.antoingmail.com>
2019-07-16 16:18:34 +0200

    Add Game relation

--- src/Entity/Platform.php
+++ src/Entity/Platform.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -30,6 +32,16 @@ class Platform
      * @ORM\Column(type="text", nullable=true)
      */
     private $presentation;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Game", mappedBy="platform", orphanRemoval=true)
+     */
+    private $games;
+
+    public function __construct()
+    {
+        $this->games = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -68,6 +80,37 @@ class Platform
     public function setPresentation(?string $presentation): self
     {
         $this->presentation = $presentation;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Game[]
+     */
+    public function getGames(): Collection
+    {
+        return $this->games;
+    }
+
+    public function addGame(Game $game): self
+    {
+        if (!$this->games->contains($game)) {
+            $this->games[] = $game;
+            $game->setPlatform($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGame(Game $game): self
+    {
+        if ($this->games->contains($game)) {
+            $this->games->removeElement($game);
+            // set the owning side to null (unless already changed)
+            if ($game->getPlatform() === $this) {
+                $game->setPlatform(null);
+            }
+        }
 
         return $this;
     }
